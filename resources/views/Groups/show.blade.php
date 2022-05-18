5e260f29777f6a0b0259743d06dbb63efe72aadc
maharaniadiputri04 <maharaniadi2002@gmail.com>
2022-05-19 03:45:52 +0700

    commit uts_maharanii

--- resources/views/Groups/show.blade.php
+++ resources/views/Groups/show.blade.php
@@ -5,9 +5,9 @@
 @section('content')
 <div class="card">
     <div class= "card-body">
-        <h3>Nama Teman : {{$friend['nama']}}</h3>
-        <h3>No Telepon Teman : {{$friend['no_telp']}}</h3>
-        <h3>Alamat Teman : {{$friend['alamat']}}</h3>
+        <h3>Nama Group : {{$group['name']}}</h3>
+        <h3>Deskripsi : {{$group['description']}}</h3> 
+       
     </div>
 </div>
 @endsection
